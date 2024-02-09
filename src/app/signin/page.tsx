1f3fb111959a6fc7de028f9fb03c7483372b96f1
Charles Obimnaeto Egesionu <egesionucharlesobimnaeto@gmail.com>
2024-02-09 09:12:55 -0800

    sign in page

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,16 @@
+"use client"
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Sign_In from "@/components/Sign-In";
+
+const SignIn = () => {
+    return ( 
+        <main>
+            <Header />
+            <Sign_In />
+            <Footer />
+        </main>
+     );
+}
+ 
+export default SignIn;
---
+++ src/components/Sign-In.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const Sign_In = () => {
+    return (
+        <main className="relative top-[60px] md:top-[75px] px-[5%] py-10 grid grid-cols-1 md:grid-cols-2 gap-6 items-center justify-between bg-white">
+            <section className="grid gap-4 text-[14px] md:text-[16px] font-normal text-dark text-center md:text-left">
+                <h1 className="text-[36px] sm:text-[38px] md:text-[48px] font-bold">Sign in to continue</h1>
+                <p className="w-full lg:w-[65%] leading-relaxed tracking-wide">Welcome back to Card Cafe credit card generator. Please sign in to continue from where you left off.</p>
+            </section>
+
+            <section className="grid place-items-center mt-6 md:mt-0">
+                <form action="" className="w-full sm:w-[95%] xl:w-[70%] grid gap-6 text-[14px] md:text-[16px] text-dark shadow shadow-yellow rounded-xl py-8 px-10">
+                    <label htmlFor="email">Email Address
+                        <input name="email" id="email" type="text" placeholder="user@gmail.com" className="input" />
+                    </label>
+                    <label htmlFor="password">Password
+                        <input name="password" id="password" type="text" placeholder="Enter Password" className="input" />
+                    </label>
+
+                    <button className="bg-gradient-to-b from-orange to-yellow hover-orange hover:shadow-2xl hover:shadow-orange transition-all delay-150 px-5 py-3 rounded-md text-white">Sign In</button>
+
+                    <p className="text-center">or</p>
+
+                    <button className="px-5 py-3 rounded-md text-dark border border-dark flex items-center justify-center gap-4 hover:shadow-2xl transition-all delay-150">
+                        <Image src={"/img/Google.png"} width={16} height={16} alt="google" />
+                        Sign In With Google
+                    </button>
+
+                    <p className="text-center">
+                        Don’t have an account? <Link href={"signup"} className="text-orange">Sign Up.</Link>
+                    </p>
+
+                </form>
+            </section>
+        </main>
+    );
+}
+
+export default Sign_In;
