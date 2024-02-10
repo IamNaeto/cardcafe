98ee9f38bc0f1785792c14bb1ddc3c4984f76957
Charles Obimnaeto Egesionu <egesionucharlesobimnaeto@gmail.com>
2024-02-10 10:02:56 -0800

    minor adjust7

--- src/components/Collections.tsx
+++ src/components/Collections.tsx
@@ -18,8 +18,8 @@ const Collections = () => {
   };
 
   return (
-    <main className="relative px-[5%] border-dark py-10 bg-white grid gap-6 text-dark text-[14px] md:text-[16px] font-medium">
-      <h1 className="text-[36px] md:text-[46px] font-bold text-center">
+    <main className="relative top-0 px-[5%] border-dark py-10 bg-white grid gap-6 text-dark text-[13px] sm:text-[14px] md:text-[16px] font-medium">
+      <h1 className="text-[34px] sm:text-[36px] md:text-[46px] font-bold text-center">
         Our Card Collections
       </h1>
 
@@ -43,22 +43,22 @@ const Collections = () => {
                 height={600}
                 alt="cardsamples"
                 loading="lazy"
-                className="w-full transition-all duration-300 ease-in-out"
+                className="w-full transition-all duration-150 ease-in-out"
               />
             </div>
 
             <div className="grid gap-4 px-2 py-4 md:p-4 border-t border-t-[#C2C2C2]">
               <div className="flex items-center justify-between gap-1 sm:gap-2">
                 <p className="flex items-center justify-center gap-1 sm:gap-2">
-                  <RiDownloadCloudLine className="text-lg sm:text-2xl" />{" "}
+                  <RiDownloadCloudLine className="text-[14px] sm:text-lg md::text-2xl" />{" "}
                   {collections.downloads}
                 </p>
                 <p className="flex items-center justify-center gap-1 sm:gap-2">
-                  <BiMessageRoundedDots className="text-lg sm:text-2xl" />{" "}
+                  <BiMessageRoundedDots className="text-[14px] sm:text-lg md::text-2xl" />{" "}
                   {collections.msg}
                 </p>
                 <p className="flex items-center justify-center gap-1 sm:gap-2">
-                  <AiOutlineEye className="text-lg sm:text-2xl" />{" "}
+                  <AiOutlineEye className="text-[14px] sm:text-lg md::text-2xl" />{" "}
                   {collections.seen}
                 </p>
               </div>
@@ -66,7 +66,7 @@ const Collections = () => {
               <div className="w-full flex items-center justify-center">
                 <Link
                   href={"/"}
-                  className="bg-gradient-to-b from-orange to-yellow hover-orange hover:shadow-2xl hover:shadow-orange transition-all delay-150 w-[95%] px-5 py-3 rounded-md text-white text-center"
+                  className="bg-gradient-to-b from-orange to-yellow hover-orange hover:shadow-2xl hover:shadow-orange transition-all delay-150 w-[95%] px-3 sm:px-5 py-1 sm:py-3 rounded-md text-white text-center"
                 >
                   View Card
                 </Link>
--- src/components/LibraryHero.tsx
+++ src/components/LibraryHero.tsx
@@ -6,7 +6,7 @@ const LibraryHero = () => {
         <main className="relative grid gap-5 hero-bg px-[5%] py-14 pt-[80px] md:pt-[120px]">
             <section className="grid grid-cols-1 md:grid-cols-2 justify-between items-center gap-5">
                 <div className="flex flex-col justify-center md:justify-center gap-4 text-[14px] md:text-[16px] text-center md:text-left text-dark font-normal">
-                    <h1 className="text-[48px] lg:text-[56px] font-extrabold">The Best Card Collection For You</h1>
+                    <h1 className="text-[46px] md:text-[48px] lg:text-[56px] font-extrabold">The Best Card Collection For You</h1>
                     <p className="w-full lg:w-[90%]">Our Credit Card Generator tool&apos;s primary purpose is for software testing and data verification purposes. Instead of using a real credit card, you can use our 100% valid credit cards to safely test your websites & apps.</p>
                 </div>
                 <div className="flex items-center justify-center mb-8 md:mb-0">
