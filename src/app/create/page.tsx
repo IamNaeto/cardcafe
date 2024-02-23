7841e6ac39fdb81eea02ca41c32222cbb95e7332
Charles Obimnaeto Egesionu <egesionucharlesobimnaeto@gmail.com>
2024-02-23 10:07:53 -0800

    backtotop icon across the pages

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import BackToTop from "@/components/BackToTop";
 import CardGen from "@/components/CardGen";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
@@ -9,6 +10,7 @@ const Create = () => {
             <Header />
             <CardGen />
             <Footer />
+            <BackToTop targetId={"header"} />
         </main>
      );
 }
--- src/app/documentation/page.tsx
+++ src/app/documentation/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import BackToTop from "@/components/BackToTop";
 import Documentations from "@/components/Documentations";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
@@ -9,6 +10,7 @@ const Documentation = () => {
             <Header />
             <Documentations />
             <Footer />
+            <BackToTop targetId={"header"} />
         </main>
      );
 }
