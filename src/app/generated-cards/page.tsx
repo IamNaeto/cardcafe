c40a241773a1c2654a97a4995ba078659485849a
Charles Obimnaeto Egesionu <egesionucharlesobimnaeto@gmail.com>
2024-03-02 09:40:35 -0800

    bug fix

--- src/app/generated-cards/page.tsx
+++ src/app/generated-cards/page.tsx
@@ -1,9 +1,11 @@
 import SideBar from "@/components/SideBar";
+import { useState } from "react";
 
 const GeneratedCards = () => {
+    const [menu, setMenu] = useState(false)
     return ( 
         <main className="w-full flex gap-10 bg-[#F5F5F5]">
-            <SideBar />
+            <SideBar  menu={menu} setMenu={setMenu}/>
 
         </main>
      );
