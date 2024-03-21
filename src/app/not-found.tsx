37f0409b32b4b477ae3e9910751e3503ce0ac6d2
Charles Obimnaeto Egesionu <egesionucharlesobimnaeto@gmail.com>
2024-03-21 06:29:19 -0700

    implment a customized error404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import Link from "next/link";
+const Error404 = () => {
+    return (
+        <main className="w-full grid grid-cols-1 md:grid-cols-2 items-center justify-center min-h-screen gap-1 md:gap-10 px-[5%] py-10">
+            <section className="w-full flex items-center justify-center">
+                <Image src="/img/error.png" width={300} height={300} alt="error-img" className="w-[80%] dangling-picture" />
+            </section>
+            <section className="flex flex-col items-center justify-center gap-4 text-[14px] md:text-[16px] text-center text-dark font-normal">
+                <h1 className="text-5xl lg:text-6xl font-extrabold">PAGE NOT FOUND</h1>
+                <p className="w-[70%]">We Couldn't Find The Page You Were Looking For...</p>
+
+                <div>
+                    <Link href={"/"} className={`bg-gradient-to-b from-orange to-yellow hover-orange hover:shadow-2xl hover:shadow-orange transition-all delay-150 w-[200px] px-5 py-3 rounded-md text-white flex items-center justify-center cursor-pointer`}
+                    >Back To Home</Link>
+                </div>
+            </section>
+        </main>
+    );
+}
+
+export default Error404;
